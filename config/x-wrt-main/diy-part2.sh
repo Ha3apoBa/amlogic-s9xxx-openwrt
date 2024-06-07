c85e8a96081ba4b33559173817b2424c7be973a1
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-06-07 21:52:11 +0800

    Update config

--- config/x-wrt-main/diy-part2.sh
+++ config/x-wrt-main/diy-part2.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 #========================================================================================================================
 # https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt for Amlogic s9xxx tv box
+# Description: Automatically Build OpenWrt
 # Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+# Source code repository: https://github.com/openwrt/openwrt / Branch: main
 #========================================================================================================================
 
 # ------------------------------- Main source started -------------------------------
 #
-# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
-# sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
-
-# Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Set etc/openwrt_release
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
-echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
-
-# Replace the default software source
-# sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
 #
 # ------------------------------- Main source ends -------------------------------
 
@@ -30,10 +24,24 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 #
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/Thaolga/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/Auroraxpm/argon package/argon
+#git clone https://github.com/douglarek/luci-app-homeproxy package/luci-app-homeproxy
+git clone https://github.com/immortalwrt/homeproxy  package/homeproxy
+git clone https://github.com/Thaolga/small-package  package/small-package
+git clone https://github.com/v2rayA/v2raya-openwrt  package/v2raya-openwrt
+git clone https://github.com/Thaolga/openwrt-passwall2  package/openwrt-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall  package/openwrt-passwall
+git clone https://github.com/linkease/istore    package/istore 
+#git clone https://github.com/fw876/helloworld   package/helloworld 
+git clone https://github.com/sirpdboy/luci-app-autotimeset.git   package/luci-app-autotimeset
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git   package/luci-app-poweroffdevice
+git clone https://github.com/sirpdboy/luci-app-ddns-go   package/luci-app-ddns-go 
+git clone https://github.com/sirpdboy/luci-app-advancedplus  package/luci-app-advancedplus 
+git clone https://github.com/sirpdboy/luci-app-partexp   package/luci-app-partexp
+# git clone https://github.com/yichya/luci-app-xray   package/luci-app-xray 
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
-
