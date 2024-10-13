3d8383042befa3cd0c82536d2617cc0995b21e22
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-10-13 23:41:05 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -27,12 +27,13 @@ rm -rf package/luci-app-amlogic
 git clone https://github.com/Thaolga/luci-app-amlogic.git package/luci-app-amlogic
 git clone https://github.com/Auroraxpm/argon package/argon
 #git clone https://github.com/douglarek/luci-app-homeproxy package/luci-app-homeproxy
-git clone https://github.com/immortalwrt/homeproxy  package/homeproxy
+git clone https://github.com/muink/luci-app-homeproxy  package/luci-app-homeproxy
 git clone https://github.com/Thaolga/small-package  package/small-package
 git clone https://github.com/v2rayA/v2raya-openwrt  package/v2raya-openwrt
 #git clone https://github.com/Thaolga/openwrt-passwall2  package/openwrt-passwall2
 git clone https://github.com/xiaorouji/openwrt-passwall  package/openwrt-passwall
 git clone https://github.com/linkease/istore    package/istore 
+git clone https://github.com/muink/openwrt-fchomo    package/openwrt-fchomo
 git clone https://github.com/morytyann/OpenWrt-mihomo    package/OpenWrt-mihomo
 git clone https://github.com/sbwml/luci-app-daed   package/dae
 #git clone https://github.com/fw876/helloworld   package/helloworld 
