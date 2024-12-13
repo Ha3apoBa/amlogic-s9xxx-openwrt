408bf13c9948c8fc7da918b88e9c0d0e3cd7b732
Auroraxpm <73517150+Auroraxpm@users.noreply.github.com>
2024-12-14 07:49:10 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -24,7 +24,34 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 #
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
-git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/Thaolga/luci-app-amlogic.git package/luci-app-amlogic
+git clone https://github.com/Auroraxpm/argon package/argon
+#git clone https://github.com/douglarek/luci-app-homeproxy package/luci-app-homeproxy
+git clone https://github.com/muink/luci-app-homeproxy  package/luci-app-homeproxy
+git clone https://github.com/Thaolga/small-package  package/small-package
+git clone https://github.com/v2rayA/v2raya-openwrt  package/v2raya-openwrt
+#git clone https://github.com/Thaolga/openwrt-passwall2  package/openwrt-passwall2
+git clone https://github.com/xiaorouji/openwrt-passwall  package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2  package/openwrt-passwall2
+git clone https://github.com/vernesong/OpenClash  package/OpenClash
+git clone https://github.com/Thaolga/openwrt-spectra package/openwrt-spectra
+git clone https://github.com/linkease/istore    package/istore 
+git clone https://github.com/muink/openwrt-fchomo    package/openwrt-fchomo
+git clone https://github.com/morytyann/OpenWrt-mihomo    package/OpenWrt-mihomo
+git clone https://github.com/sbwml/luci-app-daed   package/dae
+#git clone https://github.com/fw876/helloworld   package/helloworld 
+#git clone https://github.com/sirpdboy/luci-app-autotimeset.git   package/luci-app-autotimeset
+#git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git   package/luci-app-poweroffdevice
+#git clone https://github.com/sirpdboy/luci-app-ddns-go   package/luci-app-ddns-go 
+#git clone https://github.com/sirpdboy/luci-app-advancedplus  package/luci-app-advancedplus 
+#git clone https://github.com/sirpdboy/luci-app-partexp   package/luci-app-partexp
+git clone https://github.com/muink/luci-app-tinyfilemanager package/luci-app-tinyfilemanager
+git clone https://github.com/Thaolga/diy package/diy
+#git clone  https://github.com/Thaolga/openwrt-nekobox  package/openwrt-nekobox
+cd package/argon
+tar   xvJf  openwrt-nekobox.tar.xz
+
+# git clone https://github.com/yichya/luci-app-xray   package/luci-app-xray 
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
